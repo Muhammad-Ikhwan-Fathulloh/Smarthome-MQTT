61c45d5e8c7642d62dde2ae94bedd8bda069af45
Muhammad-Ikhwan-Fathulloh <muhammadikhwanfathulloh17@gmail.com>
2023-06-13 11:58:08 +0700

    update script.js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const client = mqtt.connect('wss://public:public@public.cloud.shiftr.io', {
+    clientId: 'javascript'
+});
+
+client.on('connect', function() {
+    console.log('connected!');
+    client.subscribe('koalawan/iot');
+});
+
+client.on('message', function(topic, message) {
+    console.log(topic + ': ' + message.toString());
+});
+
+let count = 0
+document.querySelector('button').addEventListener('click', function() {
+    count++
+    client.publish('koalawan/iot', 'hello '+count);
+});
